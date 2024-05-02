db076333afb1980d2713b6da13d85084997646f8
thisiscaro <carolinemmen@gmail.com>
2024-05-02 17:17:44 -0500

    get route to work correctly

--- api/auth/user_routes.go
+++ api/auth/user_routes.go
@@ -1,19 +1,27 @@
 package auth
 
 import (
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func AuthRoute(c *gin.Context) {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+
 	// https://dev.twitch.tv/docs/authentication/getting-tokens-oauth/#authorization-code-grant-flow
 	twitchAuthUrl := "https://id.twitch.tv/oauth2/authorize"
-	twitchRedirectURI := "/approved"          // temp. not sure i like this
-	twitchClientId := os.Getenv("TWITCH_API") // need to get this
+	twitchRedirectURI := "https://localhost:3000/" // temp. not sure i like this
+	twitchClientId := os.Getenv("TWITCH_API")      // need to get this
 
-	authUrl := twitchAuthUrl + "?client_id=" + twitchClientId + "&redirect_uri=" + twitchRedirectURI + "&response_type=code&scope=user_read"
+	println(twitchClientId)
+
+	authUrl := twitchAuthUrl + "?response_type=code&client_id=" + twitchClientId + "&redirect_uri=" + twitchRedirectURI + "&scope=user_read"
 
 	c.Redirect(http.StatusFound, authUrl)
 }
