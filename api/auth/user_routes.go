84cf12ac8ca13cefc5ec68e93f3cf5a3e3e460f3
thisiscaro <carolinemmen@gmail.com>
2024-05-01 21:27:16 -0500

    additional route fixes

--- api/auth/user_routes.go
+++ api/auth/user_routes.go
@@ -7,17 +7,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func AuthRoute() {
-	router := gin.Default()
-
+func AuthRoute(c *gin.Context) {
 	// https://dev.twitch.tv/docs/authentication/getting-tokens-oauth/#authorization-code-grant-flow
-	router.GET("/auth/twitch", func(c *gin.Context) {
-		twitchAuthUrl := "https://id.twitch.tv/oauth2/authorize"
-		twitchRedirectURI := "/approved"          // temp. not sure i like this
-		twitchClientId := os.Getenv("TWITCH_API") // need to get this
+	twitchAuthUrl := "https://id.twitch.tv/oauth2/authorize"
+	twitchRedirectURI := "/approved"          // temp. not sure i like this
+	twitchClientId := os.Getenv("TWITCH_API") // need to get this
 
-		authUrl := twitchAuthUrl + "?client_id=" + twitchClientId + "&redirect_uri=" + twitchRedirectURI + "&response_type=code&scope=user_read"
+	authUrl := twitchAuthUrl + "?client_id=" + twitchClientId + "&redirect_uri=" + twitchRedirectURI + "&response_type=code&scope=user_read"
 
-		c.Redirect(http.StatusFound, authUrl)
-	})
+	c.Redirect(http.StatusFound, authUrl)
 }
--- api/main.go
+++ api/main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"api/auth"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	router := gin.Default()
+
+	router.GET("/api", auth.AuthRoute)
+
+	router.Run(":8080")
 }
