231ca37d7678a441dc662a4664f8396e1fd423a5
thisiscaro <carolinemmen@gmail.com>
2024-05-03 17:12:02 -0500

    get useEffect to run on initial load

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,29 +1,44 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import Cookies from "js-cookie";
+import React, { useEffect, useRef } from "react";
 import { useSearchParams } from "react-router-dom";
+
+const ACCESS_TOKEN = "access_token";
+const REFRESH_TOKEN = "refresh_token";
 
 const Dashboard = () => {
   const [searchParams] = useSearchParams();
+  const isMounted = useRef(false);
   const code = searchParams.get("code");
-
-  const fetchToken = async () => {
-    try {
-      const response = await axios
-        .post("http://localhost:8080/token", {
-          code: code,
-        })
-        .catch(function (err) {
-          console.log(err);
-        });
-      console.log("response: ", response);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  console.log("code: ", code);
 
   useEffect(() => {
-    fetchToken();
-  }, []);
+    if (!isMounted.current) {
+      // Run only on initial mount
+      isMounted.current = true;
+      const fetchToken = async () => {
+        if (!code) {
+          // If code is not present, skip API call
+          return;
+        }
+        try {
+          const response = await axios.post("http://localhost:8080/token", {
+            code: code,
+          });
+          console.log("response: ", response);
+          Cookies.set(ACCESS_TOKEN, response?.data?.access_token, {
+            expires: response?.data?.expires_in,
+          });
+          Cookies.set(REFRESH_TOKEN, response?.data?.refresh_token, {
+            expires: response?.data?.expires_in,
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      fetchToken();
+    }
+  }, [code]);
 
   return (
     <div>
