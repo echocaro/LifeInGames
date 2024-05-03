a26d1ea098f5d44f21ed56788f96979428a8094c
thisiscaro <carolinemmen@gmail.com>
2024-05-03 17:42:57 -0500

    clean up auth route

--- api/auth/user_routes.go
+++ api/auth/user_routes.go
@@ -15,24 +15,19 @@ func Token(c *gin.Context) {
 		log.Fatalf("Error loading .env file: %s", err)
 	}
 
-	log.Println("what is c: ", c)
-
 	var requestBody struct {
 		Code string `json:"code"`
 	}
 	// Log incoming request body
 	if err := c.BindJSON(&requestBody); err != nil {
-		log.Println("Error parsing request body:", err)
 		c.String(http.StatusBadRequest, "Invalid request body")
-		// return
+		return
 	}
 
-	log.Println("Request Body:", requestBody)
 
 	authorizationCode := requestBody.Code
 
 	if authorizationCode == "" {
-		log.Println("Authorization code not found")
 		c.String(http.StatusBadRequest, "Authorization code not found")
 		return
 	}
@@ -44,24 +39,18 @@ func Token(c *gin.Context) {
 	twitchTokenURL += "&grant_type=authorization_code"
 	twitchTokenURL += "&redirect_uri=http://localhost:3000/home"
 
-	log.Println("URL: ", twitchTokenURL)
-
 	response, err := http.Post(twitchTokenURL, "", nil)
-	log.Println("This is the error: ", err)
+
 	if err != nil {
-		log.Println("Failed to retrieve access token:", err)
 		c.String(http.StatusInternalServerError, "Failed to retrieve access token")
 		return
 	}
 
-	log.Println("This is the response: ", response)
 	defer response.Body.Close()
 
 	responseBody, err := io.ReadAll(response.Body)
 
-	log.Println("response body: ", string(responseBody))
 	if err != nil {
-		log.Println("Failed to read response body:", err)
 		c.String(http.StatusInternalServerError, "Failed to retrieve access token")
 		return
 	}
