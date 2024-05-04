cd7362db2d3e0eaf3921d9ed9aba104082a041e4
thisiscaro <carolinemmen@gmail.com>
2024-05-04 16:25:02 -0500

    clean up dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,48 +1,14 @@
-import axios from "axios";
-import Cookies from "js-cookie";
-import React, { useEffect, useRef } from "react";
-import { useSearchParams } from "react-router-dom";
+import React from "react";
+import Games from "./Games";
 
-const ACCESS_TOKEN = "access_token";
-const REFRESH_TOKEN = "refresh_token";
+export const ACCESS_TOKEN = "access_token";
+export const REFRESH_TOKEN = "refresh_token";
 
 const Dashboard = () => {
-  const [searchParams] = useSearchParams();
-  const isMounted = useRef(false);
-  const code = searchParams.get("code");
-  console.log("code: ", code);
-
-  useEffect(() => {
-    if (!isMounted.current) {
-      // Run only on initial mount
-      isMounted.current = true;
-      const fetchToken = async () => {
-        if (!code) {
-          // If code is not present, skip API call
-          return;
-        }
-        try {
-          const response = await axios.post("http://localhost:8080/token", {
-            code: code,
-          });
-          console.log("response: ", response);
-          Cookies.set(ACCESS_TOKEN, response?.data?.access_token, {
-            expires: response?.data?.expires_in,
-          });
-          Cookies.set(REFRESH_TOKEN, response?.data?.refresh_token, {
-            expires: response?.data?.expires_in,
-          });
-        } catch (err) {
-          console.log(err);
-        }
-      };
-      fetchToken();
-    }
-  }, [code]);
-
   return (
     <div>
       <h2>Dashboard</h2>
+      <Games />
     </div>
   );
 };
