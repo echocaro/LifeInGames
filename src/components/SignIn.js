74ff5703eb3c953ee378d3456c0d113117e4f078
thisiscaro <carolinemmen@gmail.com>
2024-05-03 20:18:00 -0500

    new sign in page

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,11 +4,10 @@ const SignIn = () => {
   return (
     <div>
       <h3>Hello</h3>
-      <a
-        href={`https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=${process.env.REACT_APP_TWITCH_API}&redirect_uri=http://localhost:3000/home&scope=user_read`}
-      >
-        Connect with twitch
-      </a>
+      <input
+        title="Enter your steamdid"
+        placeholder="Example: 76561197960435530"
+      />
     </div>
   );
 };
