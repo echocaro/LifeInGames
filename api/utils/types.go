65935c592eac770580a89584fa4e91b644b4e1d3
thisiscaro <carolinemmen@gmail.com>
2024-05-04 15:16:34 -0500

    move types

---
+++ api/utils/types.go
@@ -0,0 +1,23 @@
+package utils
+
+type GameInfo struct {
+	AppID     int    `json:"appid"`
+	Name      string `json:"name"`
+	Playtime  int    `json:"playtime_forever"`
+	ImageURL string
+}
+
+type GameData struct {
+	Name string `json:"name"`
+	ImageUrl string
+	Message string
+}
+
+type TopGenreGameInfo struct {
+	Name string `json:"name"`
+	Genre []GenreInfo
+}
+
+type GenreInfo struct {
+    Name string `json:"name"`
+}
