59de2c4d5e12f0565a6187774a985c5d4670cd36
thisiscaro <carolinemmen@gmail.com>
2024-05-04 16:25:26 -0500

    display data and hit right endpoint

---
+++ src/components/Games.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import Cookies from "js-cookie";
+
+const Games = () => {
+  const [games, setGames] = useState([]);
+  const steamId = Cookies.get("steamdId");
+  const isMounted = useRef(false);
+
+  useEffect(() => {
+    if (!isMounted.current) {
+      isMounted.current = true;
+      const fetchToken = async () => {
+        if (!steamId) {
+          return;
+        }
+        try {
+          const response = await axios.get(
+            `http://localhost:8080/${steamId}/top-games`,
+            {
+              steamId: steamId,
+            }
+          );
+          setGames(response?.data);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      fetchToken();
+    }
+  }, [steamId]);
+
+  return (
+    <div>
+      <h3 className="">Your top games are </h3>
+      <div className="flex flex-row flex-wrap justify-center">
+        {games.map((game) => (
+          <div className="m-1">
+            <img
+              src={game.ImageURL}
+              alt=""
+              className="rounded-3xl w-48 h-auto"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Games;
