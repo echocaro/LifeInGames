c52ec59ff25ab45af2853c817261adfe78b38c28
thisiscaro <carolinemmen@gmail.com>
2024-05-23 21:43:45 -0500

    update main.go

--- api/main.go
+++ api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"api/endpoints"
+	"net/http"
 	"time"
 
 	"github.com/gin-contrib/cors"
@@ -12,13 +13,21 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
-		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
-		AllowHeaders:     []string{"Content-Type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Authorization"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
+			AllowOrigins:     []string{"http://localhost:3000", "https://www.thegamerswave.com"},
+			AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+			AllowHeaders:     []string{"Content-Type", "Authorization", "Access-Control-Allow-Origin"},
+			AllowCredentials: true,
+			MaxAge:           12 * time.Hour,
 	}))
 
+
+	router.OPTIONS("/*any", func(c *gin.Context) {
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Access-Control-Allow-Origin")
+			c.Status(http.StatusOK)
+	})
+
+	router.GET("/", endpoints.HealthCheck)
 	router.GET("/:steamId/games", endpoints.OwnedGames)
 	router.GET("/:steamId/games-data", endpoints.GamePlayData)
 	router.GET("/:steamId/top-games", endpoints.GetTopGames)
